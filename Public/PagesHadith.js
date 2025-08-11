0b3135dccef6f3ce8e13ea0793767ffba241583c
chagochagokhan <chagochagokhan@gmail.com>
2025-08-11 08:55:55 +0500

    Create PagesHadith.js

---
+++ Public/PagesHadith.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Hadith() {
+  return (
+    <div>
+      <h2>حدیث سیکشن</h2>
+      <p>یہاں پر مستند احادیث شامل ہوں گی۔</p>
+    </div>
+  );
+}
