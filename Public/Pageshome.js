591d8aed01375cff3a15614ec269420958f6a07c
chagochagokhan <chagochagokhan@gmail.com>
2025-08-11 08:52:22 +0500

    Create Pageshome.js

---
+++ Public/Pageshome.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <h2>خوش آمدید — نور اور دنیا</h2>
+      <p>یہ ویب سائٹ قرآن، حدیث، اسلامی ڈرامے اور معلومات کا مرکز ہے۔</p>
+    </div>
+  );
+}
