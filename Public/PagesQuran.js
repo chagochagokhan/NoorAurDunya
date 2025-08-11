28eea77eaa1a7cb7b23ae2eeccd93de7402e92f0
chagochagokhan <chagochagokhan@gmail.com>
2025-08-11 08:53:20 +0500

    Create PagesQuran.js

---
+++ Public/PagesQuran.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Quran() {
+  return (
+    <div>
+      <h2>قرآن سیکشن</h2>
+      <p>یہاں پر قرآنی آیات اور ترجمہ شامل ہوگا۔</p>
+    </div>
+  );
+}
