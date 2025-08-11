156f31484bf6cc6d0ae86b60b4cc0dd0afde8cb2
chagochagokhan <chagochagokhan@gmail.com>
2025-08-11 08:56:50 +0500

    Create Pages contect.js

---
+++ Public/Pages contect.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Contact() {
+  return (
+    <div>
+      <h2>رابطہ</h2>
+      <p>ہمارے ساتھ رابطہ کرنے کے لیے ای میل کریں: info@nooraurdunya.com</p>
+    </div>
+  );
+}
